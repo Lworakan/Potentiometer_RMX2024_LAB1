6b6bfae629409babfe7381c59d8a8c9afff8623e
Worakan Lasudee <worakan.lasudee@gmail.com>
2024-10-09 11:08:18 +0700

    Lab2

---
+++ potenlab_ert_rtw/potenlab_private.h
@@ -0,0 +1,36 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: potenlab_private.h
+ *
+ * Code generated for Simulink model 'potenlab'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
+ * C/C++ source code generated on : Tue Oct  8 01:22:17 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex-M
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef potenlab_private_h_
+#define potenlab_private_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+#include "potenlab_types.h"
+
+/* Private macros used by the generated code to access rtModel */
+#ifndef rtmSetTFinal
+#define rtmSetTFinal(rtm, val)         ((rtm)->Timing.tFinal = (val))
+#endif
+#endif                                 /* potenlab_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
